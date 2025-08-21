a47343ae279a7074018e36b897a7290170c199d2
lootboi <katora@dons.usfca.edu>
2025-08-21 17:03:42 -0400

    fix: descrepencies between quoting-engine

---
+++ src/protocols/four-meme/index.ts
@@ -0,0 +1,6 @@
+export { FourMemeService } from './four-meme.service';
+export type {
+  FourMemeConfig,
+  FourMemePriceResponse,
+  FourMemeQuoteResponse,
+} from './four-meme.types';
--- src/types/enums.ts
+++ src/types/enums.ts
@@ -14,6 +14,7 @@ export enum ProtocolEnum {
 }
 
 export enum ChainIdEnum {
+  UNKNOWN = 0,
   SOLANA = 1399811149,
   AVALANCHE = 43114,
   SONIC = 146,
